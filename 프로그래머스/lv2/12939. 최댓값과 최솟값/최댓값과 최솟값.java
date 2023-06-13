cc1866bfb83154f9c6dcf26c897ed7a305d800d9
ShinChanCoding <87774238+ShinChanCoding@users.noreply.github.com>
2023-06-13 16:59:16 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,17 +2,14 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         String[] splitNumber = s.split(" ");
-        int big = Integer.parseInt(splitNumber[0]);
-        int small = Integer.parseInt(splitNumber[0]);
+        int big,small;
+        small = big = Integer.parseInt(splitNumber[0]);
         for(int i = 0; i < splitNumber.length; i++){
             if(Integer.parseInt(splitNumber[i]) > big ){
                 big  = Integer.parseInt(splitNumber[i]);
             }
-        }
-        small = big;
-        for(int j = 0; j < splitNumber.length; j++){
-            if(Integer.parseInt(splitNumber[j]) < small){
-                small = Integer.parseInt(splitNumber[j]);
+            if(Integer.parseInt(splitNumber[i]) < small){
+                small = Integer.parseInt(splitNumber[i]);
             }
         }
         answer = "" + small + " " + big;
