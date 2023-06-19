bf427813c33da1bf12df264492f967642fc86ca5
ShinChanCoding <87774238+ShinChanCoding@users.noreply.github.com>
2023-06-19 23:43:37 +0900

    [level 2] Title: 구명보트, Time: 8.75 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int j = 0;
+        for(int i = people.length-1; i >= j; i--){
+            if(people[i] + people[j] <= limit){
+                j++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
