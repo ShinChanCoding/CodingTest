1c352a8665d7d9841a086db72b9192a192086c51
ShinChanCoding <87774238+ShinChanCoding@users.noreply.github.com>
2023-07-05 01:42:44 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int a, int b) {
+        int round = 1;
+
+        while (true) {
+            if (Math.abs(a - b) == 1 && Math.min(a, b) % 2 == 1) {
+                break;
+            }
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            round++;
+        }
+
+        return round;
+    }
+}
