7d5b5e8d0a0a60da91f8e658df6d72f7ff7d0a6d
ShinChanCoding <87774238+ShinChanCoding@users.noreply.github.com>
2023-07-15 23:46:46 +0900

    Update 크기가 작은 부분문자열.java

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,24 +1,22 @@
 import java.util.*;
 class Solution {
+    private int answer = 0;
     public int solution(String t, String p) {
-        int answer = 0;
         for(int i = 0; i < t.length(); i++){
             if(i + p.length() == t.length()){
-                String k = t.substring(i, i + p.length());
-                Long num1 = Long.parseLong(k);
-                Long num2 = Long.parseLong(p);
-                if(num1 <= num2){
-                    answer++;
-                }
+                addBigger(i, p, t);
                 break;
             }
-            String k = t.substring(i, i + p.length());
-            Long num1 = Long.parseLong(k);
-            Long num2 = Long.parseLong(p);
-            if(num1 <= num2){
-                answer++;
-            }
+            addBigger(i, p, t);
         }
         return answer;
+    }
+    public void addBigger(int i, String p, String t){
+        String k = t.substring(i, i + p.length());
+        Long num1 = Long.parseLong(k);
+        Long num2 = Long.parseLong(p);
+        if(num1 <= num2){
+            answer++;
+        }
     }
 }
