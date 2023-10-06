9a04b68821a399b1948dfebda878657058a4e5c7
EunYeop Lee <juju6050@naver.com>
2023-10-06 17:08:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.25 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
+                answer = entry.getKey();
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
