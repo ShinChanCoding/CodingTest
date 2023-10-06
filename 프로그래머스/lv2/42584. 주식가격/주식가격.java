a0c14e32223557eec1bdacffe577d2d1ec030a33
EunYeop Lee <juju6050@naver.com>
2023-10-06 15:52:44 +0900

    [level 2] Title: 주식가격, Time: 8.96 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -3,22 +3,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-
         for (int i = 0; i < prices.length; i++) {
             int price = prices[i];
             int duration = 0;
-
             for (int j = i + 1; j < prices.length; j++) {
                 duration++;
-
                 if (price > prices[j]) {
                     break;
                 }
             }
-
             answer[i] = duration;
         }
-
         return answer;
     }
 }
