00d78cb25c35de9ddc340cf3f0265c966d2a4ffc
EunYeop Lee <juju6050@naver.com>
2024-11-02 09:09:37 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] result = new boolean[n];
+        for(int i = 0; i < n; i++){
+            if(!result[i]){
+                bfs(computers, i, result);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    boolean[] bfs(int[][] computers, int i, boolean[] result){
+        result[i] = true;
+        for(int j = 0; j < computers.length; j++){
+            if(computers[i][j] == 1 && result[j] == false){
+                bfs(computers, j, result);
+            }
+        }
+        return result;
+    }
+}
